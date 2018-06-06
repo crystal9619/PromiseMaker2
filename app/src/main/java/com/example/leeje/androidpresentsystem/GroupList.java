490f09a8f2999ab86ed4861164c50907362f8819
leejieun94 <xaq03061@gmail.com>
2018-06-06 11:40:25 +0900

    Merge branch '구리숙달' of https://github.com/crystal9619/PromiseMaker2 into chat
    # Conflicts:
    #	app/build.gradle
    닉네임과 uid database에 등록

---
+++ app/src/main/java/com/example/leeje/androidpresentsystem/GroupList.java
@@ -0,0 +1,80 @@
+package com.example.leeje.androidpresentsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class GroupList extends AppCompatActivity {
+
+    private ListView group_list1;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grouplist);
+        Intent intent = new Intent(GroupList.this,makeGroupName.class);
+        Log.e("text","list map");
+        group_list1 = (ListView) findViewById(R.id.group_list);
+
+           final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+            group_list1.setAdapter(adapter);
+            final FirebaseUser mUser= FirebaseAuth.getInstance().getCurrentUser();
+                    Log.e("text","get uid");
+                    String uid=mUser.getUid();
+        databaseReference.child("UID").child("uid_group").child(uid).child("group").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Log.e("LOG", "dataSnapshot.getKey() : " + dataSnapshot.getKey());
+                adapter.add(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+        });
+    }
+}
+
+
+
+
+
